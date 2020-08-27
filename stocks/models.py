4770361603471dd9d36021aa63de880c654222a0
LeoKnox <LeoKnox@users.noreply.github.com>
2020-08-26 22:01:26 -0700

    Add files via upload

--- stocks/models.py
+++ stocks/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Room(models.Model):
     LIGHT = [('on', 'On'), ('off', 'Off')]
     name = models.CharField(max_length=50)
-    floor = models.CharField(max_lenght=50)
+    floor = models.CharField(max_length=50)
     description = models.TextField(blank=True)
     width = models.IntegerField();
     length = models.IntegerField();
@@ -14,5 +14,5 @@ class Room(models.Model):
 class Door(models.Model):
     WALL = [('N', 'North'),('E', 'East'),('S', 'South'),('W','West')]
     material = models.CharField(max_length=50)
-    wall = models.CharField(max_lenght=1, choices="WALL")
+    wall = models.CharField(max_length=1, choices=WALL)
     location = models.IntegerField();
