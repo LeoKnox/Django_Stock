4b767cc96d9d192b9a9c48600b4caef00e77069c
LeoKnox <LeoKnox@users.noreply.github.com>
2020-08-26 21:43:28 -0700

    Add files via upload

--- stocks/models.py
+++ stocks/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
-class Room(models):
+class Room(models.Model):
+    LIGHT = [('on', 'On'), ('off', 'Off')]
     name = models.CharField(max_length=50)
     floor = models.CharField(max_lenght=50)
     description = models.TextField(blank=True)
     width = models.IntegerField();
-    length = models.InterField();
+    length = models.IntegerField();
+    lighting = models.CharField(max_length=3, choices=LIGHT)
     submission_date = models.DateTimeField()
+    doors = models.ManyToManyField('Door')
+
+class Door(models.Model):
+    WALL = [('N', 'North'),('E', 'East'),('S', 'South'),('W','West')]
+    material = models.CharField(max_length=50)
+    wall = models.CharField(max_lenght=1, choices="WALL")
+    location = models.IntegerField();
